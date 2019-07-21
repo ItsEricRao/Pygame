54d48b88e666152fce50fc1bc3706b88837b5971
Rao_ <49305406+ItsEricRao@users.noreply.github.com>
2019-07-21 21:41:28 +0800

    Finish Flappy Bird
    EN english
    Finally finish the game
    How to play:
    [space] to fly
    [R] to restart (when u die in the game u can use that)
    
    Thanks teacher Leng Qian to help me fix the bug.
    
    and because the pyinstaller doesn't work for pygame, so i just posted the orginal code, u can open in your IDE install the pygame using pip, and run it. Most time i use Pycharm, u can download right here: https://www.jetbrains.com/pycharm/ , and don't forget download python: https://www.python.org/ .
    
    PS: REMEMBER TO DOWNLOAD THE WHOLE PRPOJECT! WHOLE PROJECT, BECAUSE THE IMAGE INSIDE THE GAME IS ALSO INSIDE THE PACKAGE!
    
    ZH 简体中文
    终于写完了游戏
    怎么玩：
    按下【空格】来飞行
    按下【R】来重玩（只有你死了才可以重新玩）
    
    谢谢冷前老师帮我修复bug
    
    还有因为pyinstaller不可以在有pygame的代码上运行，所以我放出了源码，你可以用你的IDE打开然后用pip安装pygame，然后运行。我一般使用Pycharm，你可以在这里下载：https://www.jetbrains.com/pycharm/，也不要忘记下载python：https://www.python.org/。
    
    附：一定要下载整个项目！整个项目，因为游戏里的图片素材也是在这个包里！

--- Pygame/Flappy Bird/FlappyBird.py
+++ Pygame/Flappy Bird/FlappyBird.py
@@ -2,26 +2,39 @@ import pygame
 from pygame.locals import *
 from sys import exit
 from random import randint
+
+
+def text_image(name, size, text, color):
+    # create new system font
+    font = pygame.font.SysFont(name, size)
+    # print the text
+    image = font.render(text, True, color)
+    return image
 # PATH
 path = '../../resources/images/'
 index = 0
 y = 300
-# set rectangle parameter
-color = (100, 200, 0)
-left = 200
-top = 0
-width = 60
-x_distance = 300
-y_distance = 150
+score = 0
+flag = False
 rect1 = []
 rect2 = []
-for i in range(10):
-    height = randint(200, 400)
-    rect1.append([left, top, width, height])
-    rect2.append([left, height + y_distance, width, 600 - height - y_distance])
-    left = left + x_distance
-    x_distance = x_distance - 10
-    y_distance = y_distance - 5
+# set rectangle parameter
+color = (100, 200, 0)
+def layout():
+    left = 200
+    top = 0
+    width = 60
+    x_distance = 300
+    y_distance = 150
+    for i in range(10):
+        height = randint(200, 400)
+        rect1.append([left, top, width, height])
+        rect2.append([left, height + y_distance, width, 600 - height - y_distance])
+        left = left + x_distance
+        x_distance = x_distance - 10
+        y_distance = y_distance - 5
+
+layout()
 # initallizing
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
@@ -35,7 +48,10 @@ birds = [
         path + '1.png'),
     pygame.image.load(
         path+ '2.png')]
+over = pygame.image.load(path + 'over.png')
 pygame.key.set_repeat(1, 1)
+clock = pygame.time.Clock()
+clock.tick(30)
 # when it is running...
 while True:
     for event in pygame.event.get():
@@ -45,18 +61,51 @@ while True:
             key = event.key
             if key == K_SPACE:
                 y = y - 5
+            if key == K_r:
+                if flag:
+                    flag = False
+                    layout()
+                    y = 300
+                    score = 0
     pygame.display.update()
     screen.blit(bg, (0, 0))
+    # game over background
+    if flag or score == 10:
+        flag = True
+        rect1 = []
+        rect2 = []
+        screen.blit(over, (300, 270))
+        for i in range(score):
+            screen.blit(birds[0], (i * 60 + 100, 200))
+        pygame.display.update()
+        continue
     # draw rectangle
-    for i in range(10):
+    for i in range(len(rect1)):
         pygame.draw.rect(screen, color, rect1[i])
         pygame.draw.rect(screen, color, rect2[i])
-        rect1[i][0] = rect1[i][0] - 1
-        rect2[i][0] = rect2[i][0] - 1
+        rect1[i][0] = rect1[i][0] - 4
+        rect2[i][0] = rect2[i][0] - 4
         screen.blit(birds[index], (0, y))
     y = y + 2
     if index == 2:
         index = 0
     else:
         index = index + 1
-        pygame.display.update()
+    '''
+    Determine whether the list of rectangular obstacles is empty, and just move out
+    of the game window will move the upper and lower rectangular obstacles, score 
+    plus one.
+    '''
+    if rect1 and rect1[0][0] <= -60:
+        rect1.pop(0)
+        rect2.pop(0)
+        score = score + 1
+    # collision detection
+    if rect1:
+        if (rect1[0][0] < 48 and rect1[0][3] > y) or (rect2[0][0] < 48 and rect2
+        [0][1] < y + 48):
+            flag = True
+    # create score
+    img = text_image(None, 50, str(score), (255, 255, 255))
+    screen.blit(img, (20, 20))
+    pygame.display.update()
